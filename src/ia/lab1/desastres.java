0cb15fcf9f720de1b5b3fd34353cee0cc9d5210b
Marc Cobos <coboshernandez@gmail.com>
2014-10-01 17:48:27 +0200

    initial structure lab1

---
+++ src/ia/lab1/desastres.java
@@ -0,0 +1,16 @@
+package ia.lab1;
+
+import IA.Desastres.Grupo;
+
+public class desastres {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Prova per veure que els jars estan ben linkats
+		Grupo g = new Grupo(10,20,30,40);
+		System.out.print(g.getCoordY());
+	}
+
+}
